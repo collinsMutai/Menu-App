268af822b1df2c45d7ff785df5f565e2f12cea99
Collins Mutai <collinsfrontend@gmail.com>
2020-07-12 17:43:15 +0300

    React Router

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -6,7 +6,7 @@ const Menu = (props) => {
   const menu = props.dishes.map((dish) => {
     return (
       <div className="col-12 col-md-5 m-1" key={dish.id}>
-        <Card onClick={() => props.onClick(dish.id)}>
+        <Card>
           <CardImg width="100%" src={dish.image} alt={dish.name} />
           <CardImgOverlay>
             <CardTitle>{dish.name}</CardTitle>
