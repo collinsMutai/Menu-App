fe2aebb8b9d67c81ed335f0cf50751d5d85abf9e
Collins Mutai <collinsfrontend@gmail.com>
2020-07-20 16:02:52 +0300

    Intro. to Redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -24,7 +24,6 @@ class Header extends Component {
   constructor(props) {
     super(props);
 
-    this.toggleNav = this.toggleNav.bind(this);
     this.state = {
       isNavOpen: false,
       isModalOpen: false,
